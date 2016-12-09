60497e70ccc414458eff44f5815b4825d9aa9c1b
Andy Tarbard <andy@tarbard.net>
2016-12-09 15:12:23 +0000

    Added S3 test lambda

--- lambda/s3test.py
+++ lambda/s3test.py
@@ -4,8 +4,23 @@ import json
 def lambda_handler(event, context):
 
     testdata = [
-        'a': 'aaa',
-        'b': 'bbb',
-        'c': 'ccc',
-        'd': 'ddd'
+        {'a': 'aaa'},
+        {'a': 'bbb'},
+        {'a': 'ccc'},
+        {'a': 'ddd'}
     ]
+
+    s3 = boto3.resource('s3')
+
+    outputObject = s3.Object('techdebt-website', 'processing/andytest')
+    outputbuffer = ""
+
+    for i in testdata:
+        outputbuffer = outputbuffer + i['a']   
+
+    outputbytes=bytearray(outputbuffer)
+    print(outputbytes)
+    outputObject.put(Body=outputbytes)
+
+
+lambda_handler("","")
