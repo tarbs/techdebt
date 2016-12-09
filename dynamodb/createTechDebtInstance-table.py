8a3e8d4dbb6ab9e1633d7d99565ee175306da814
Andy Tarbard <andy@tarbard.net>
2016-12-09 09:03:40 +0000

    Added test data loading to table creation scripts

--- dynamodb/createTechDebtInstance-table.py
+++ dynamodb/createTechDebtInstance-table.py
@@ -6,36 +6,59 @@ import boto3
 
 tablename = 'TechDebtInstance'
 
+testdata = [
+    {
+        'ProductID': 'RDS::MySQL',
+        'InstanceID': 'andytest-MySQL-instance-1',
+        'VersionID': '5.7.61',
+        'LastRecordedDatetime': '20161208085000'
+    }
+]
+
 dynamodb = boto3.resource('dynamodb')
 
-table = dynamodb.create_table(
-    TableName=tablename,
-    KeySchema=[
-        {
-            'AttributeName': 'ProductID',
-            'KeyType': 'HASH'
-        },
-        {
-            'AttributeName': 'InstanceID',
-            'KeyType': 'RANGE'
-        }
-    ],
-    AttributeDefinitions=[
-        {
-            'AttributeName': 'ProductID',
-            'AttributeType': 'S'
-        },
-        {
-            'AttributeName': 'InstanceID',
-            'AttributeType': 'S'
-        }
-    ],
-    ProvisionedThroughput={
-        'ReadCapacityUnits': 5,
-        'WriteCapacityUnits': 5
-    }
-)
 
-table.meta.client.get_waiter('table_exists').wait(TableName=tablename)
+table = dynamodb.Table(tablename)
+
+if table is None:
+    table = dynamodb.create_table(
+        TableName=tablename,
+        KeySchema=[
+            {
+                'AttributeName': 'ProductID',
+                'KeyType': 'HASH'
+            },
+            {
+                'AttributeName': 'InstanceID',
+                'KeyType': 'RANGE'
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'ProductID',
+                'AttributeType': 'S'
+            },
+            {
+                'AttributeName': 'InstanceID',
+                'AttributeType': 'S'
+            }
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 5,
+            'WriteCapacityUnits': 5
+        }
+    )
+
+    table.meta.client.get_waiter('table_exists').wait(TableName=tablename)
+
+print(table.item_count)
+
+for item in testdata:
+    returnval = table.put_item(
+        Item=item,
+        ReturnValues='ALL_OLD'
+    )
+    print(returnval)
+
 
 print(table.item_count)
--- dynamodb/createTechDebtReference-table.py
+++ dynamodb/createTechDebtReference-table.py
@@ -6,36 +6,59 @@ import boto3
 
 tablename = 'TechDebtReference'
 
+testdata = [
+    {
+        'ProductID': 'RDS::MySQL',
+        'InstanceID': 'andytest-MySQL-instance-1',
+        'VersionID': '5.7.61',
+        'LastRecordedDatetime': '20161208085000'
+    }
+]
+
 dynamodb = boto3.resource('dynamodb')
 
-table = dynamodb.create_table(
-    TableName=tablename,
-    KeySchema=[
-        {
-            'AttributeName': 'ProductID',
-            'KeyType': 'HASH'
-        },
-        {
-            'AttributeName': 'VersionID',
-            'KeyType': 'RANGE'
-        }
-    ],
-    AttributeDefinitions=[
-        {
-            'AttributeName': 'ProductID',
-            'AttributeType': 'S'
-        },
-        {
-            'AttributeName': 'VersionID',
-            'AttributeType': 'S'
-        }
-    ],
-    ProvisionedThroughput={
-        'ReadCapacityUnits': 5,
-        'WriteCapacityUnits': 5
-    }
-)
+table = dynamodb.Table(tablename)
 
-table.meta.client.get_waiter('table_exists').wait(TableName=tablename)
+if table is None:
+
+    table = dynamodb.create_table(
+        TableName=tablename,
+        KeySchema=[
+            {
+                'AttributeName': 'ProductID',
+                'KeyType': 'HASH'
+            },
+            {
+                'AttributeName': 'VersionID',
+                'KeyType': 'RANGE'
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'ProductID',
+                'AttributeType': 'S'
+            },
+            {
+                'AttributeName': 'VersionID',
+                'AttributeType': 'S'
+            }
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 5,
+            'WriteCapacityUnits': 5
+        }
+    )
+
+    table.meta.client.get_waiter('table_exists').wait(TableName=tablename)
+
+print(table.item_count)
+
+for item in testdata:
+    returnval = table.put_item(
+        Item=item,
+        ReturnValues='ALL_OLD'
+    )
+    print(returnval)
+
 
 print(table.item_count)
