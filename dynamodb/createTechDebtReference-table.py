d7df4cd85cdb1596bf6d28495b284f88466268c8
Andy Tarbard <andy@tarbard.net>
2016-12-09 10:20:31 +0000

    Updates to RDS test data

--- dynamodb/createTechDebtReference-table.py
+++ dynamodb/createTechDebtReference-table.py
@@ -7,15 +7,16 @@ import boto3
 tablename = 'TechDebtReference'
 
 testdata = [
-    {
-        'ProductID': 'RDS::MySQL',
-        'VersionID': '5.7.61',
-        'InceptionDate': '20130101',
-        'ExpectedEoLDate': '20170330',
-        'ForcedUpgrade': True,
-        'LastRecordedDatetime': '20161208085000',
-        'Comments': 'This is a classic'
-    }
+    {'ProductID': 'RDS::MySQL','VersionID': '5.7.61','InceptionDate': '20150101','ExpectedEoLDate': '20160623','ForcedUpgrade': True,'LastRecordedDatetime': '20161208085000','Comments': 'The best'},
+    {'ProductID': 'RDS::MySQL','VersionID': '5.5.40a','InceptionDate': '20120101','ExpectedEoLDate': '20150330','ForcedUpgrade': True,'LastRecordedDatetime': '20161208085000','Comments': "Ooooh, that's old"},
+    {'ProductID': 'RDS::MySQL','VersionID': '5.6.27','InceptionDate': '20140101','ExpectedEoLDate': '20170330','ForcedUpgrade': True,'LastRecordedDatetime': '20161208085000','Comments': "A classic"},
+    {'ProductID': 'RDS::PostgreSQL','VersionID': '9.6.1','InceptionDate': '20130101','ExpectedEoLDate': '20170330','ForcedUpgrade': True,'LastRecordedDatetime': '20161208085000','Comments': " "},
+    {'ProductID': 'RDS::PostgreSQL','VersionID': '9.3.9','InceptionDate': '20130101','ExpectedEoLDate': '20170330','ForcedUpgrade': True,'LastRecordedDatetime': '20161208085000','Comments': " "},
+    {'ProductID': 'RDS::PostgreSQL','VersionID': '9.3.1','InceptionDate': '20130101','ExpectedEoLDate': '20170330','ForcedUpgrade': True,'LastRecordedDatetime': '20161208085000','Comments': " "},
+    {'ProductID': 'RDS::SQLServer::Express','VersionID': '10.50.2789.0.v1','InceptionDate': '20100101','ExpectedEoLDate': '20140330','ForcedUpgrade': True,'LastRecordedDatetime': '20161208085000','Comments': " "},
+    {'ProductID': 'RDS::SQLServer::Express','VersionID': '12.00.4422.0.v1','InceptionDate': '20100101','ExpectedEoLDate': '20140330','ForcedUpgrade': True,'LastRecordedDatetime': '20161208085000','Comments': " "},
+    {'ProductID': 'RDS::SQLServer::Express','VersionID': '12.00.5000.0.v1','InceptionDate': '20100101','ExpectedEoLDate': '20140330','ForcedUpgrade': True,'LastRecordedDatetime': '20161208085000','Comments': " "},
+    {'ProductID': 'RDS::SQLServer::Express','VersionID': '13.00.2164.0.v1','InceptionDate': '20100101','ExpectedEoLDate': '20140330','ForcedUpgrade': True,'LastRecordedDatetime': '20161208085000','Comments': " "}
 ]
 
 dynamodb = boto3.resource('dynamodb')
