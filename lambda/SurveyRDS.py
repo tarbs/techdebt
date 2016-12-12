cf2725533c6874ea87f650fe21660176dbf9e6d0
Andy Tarbard <andy@tarbard.net>
2016-12-12 20:05:07 +0000

    Added SurveyRDS.py population lambda

--- lambda/SurveyRDS.py
+++ lambda/SurveyRDS.py
@@ -2,6 +2,7 @@
 # Survey the current AWS account for running RDS instances and log the details into DynamoDB
 
 import boto3
+from boto3.dynamodb.conditions import Key, Attr
 import json
 from datetime import datetime
 
@@ -12,18 +13,18 @@ def lambda_handler(event, context):
     tablename = 'TechDebtInstance'
     dateFormat = "%Y%m%d%H%M%S"
     dateStamp = datetime.utcnow().strftime(dateFormat)
-    
+
     # Connect to AWS account via boto
     rds = boto3.client('rds')
     dynamodb = boto3.resource('dynamodb')
-    
+
     table = dynamodb.Table(tablename)
     rdsInstances = rds.describe_db_instances()
-    
+
     # extract the instance details and write to the table 
-    
+
     for instance in rdsInstances['DBInstances']:
-        # print(instance['DBInstanceIdentifier'] + ' ' +  instance['Engine'] + ' ' +  instance['EngineVersion'])
+
         productName = 'RDS::' + instance['Engine']
         instanceItem = {
             'ProductID': productName,
@@ -32,16 +33,39 @@ def lambda_handler(event, context):
             'LastRecordedDatetime': dateStamp
         }
         # print(json.dumps(instanceItem))
-    
+
+        print('Adding ' + productName + '::' + instance['DBInstanceIdentifier'] )
+
         returnval = table.put_item(
-            Item=instanceItem,
-            ReturnValues='ALL_OLD'
+            Item=instanceItem
         )
-        print(returnval)
-    
+
+
+    # Find all RDS instances that were not modified today
+
+    instancesToDelete = table.query(
+        ProjectionExpression="ProductID, InstanceID",
+        KeyConditionExpression=Key('ProductID').eq(productName),
+        FilterExpression='LastRecordedDatetime < :ts',
+        ExpressionAttributeValues={
+            ":ts": dateStamp
+        }
+    )
+
+    for instance in instancesToDelete['Items']:
+
+        print('Deleting ' + productName + '::' + instance['InstanceID'])
+        table.delete_item(
+            Key={
+                'ProductID': productName,
+                'InstanceID': instance['InstanceID']
+            }
+        )
 
 # TODO
 #
+
 # Survey db clusters  - for Aurora
 
-
+# For local test execution
+lambda_handler(0,0)
